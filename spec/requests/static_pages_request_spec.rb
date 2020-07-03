44cdc7e6ac61ec5d277af60b739516cb439c6b11
Nagai Kensuke <kn.10n9@gmail.com>
2020-07-03 16:48:08 +0900

    Finish static pages

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 
-  describe "GET /home" do
+
+let(:base_title) { 'Shokku Takku' }
+
+  describe "Home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
+      assert_select "title", "Home | #{base_title}"
     end
   end
 
