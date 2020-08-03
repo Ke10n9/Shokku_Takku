cbff883c7fac66a823dbca69fbfd4d3abbc9e34c
Nagai Kensuke <kn.10n9@gmail.com>
2020-08-03 22:30:36 +0900

    Add delete user test to users_index_spec

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -13,8 +13,9 @@ RSpec.feature "UsersIndices", type: :feature do
       log_in_as @admin
     }
 
-    scenario "visit users_path" do
+    scenario "visit users_path and delete user" do
       visit users_path
+      expect(page).to have_current_path '/users'
       expect(page).to have_selector("div.pagination", count: 2)
       User.paginate(page: 1).each do |user|
         expect(page).to have_link(user.name, href: user_path(user))
@@ -22,6 +23,9 @@ RSpec.feature "UsersIndices", type: :feature do
           expect(page).to have_link("delete", href: user_path(user))
         end
       end
+      expect do
+        first(:link, 'delete').click
+      end.to change(User, :count).by(-1)
     end
   end
 end
