43edd232c3ffc81cd837d4e89544b9582371f9f1
Nagai Kensuke <kn.10n9@gmail.com>
2020-08-07 15:27:27 +0900

    Add invalid dish test to registeration test of MenusInterface

--- spec/features/menus_interface_spec.rb
+++ spec/features/menus_interface_spec.rb
@@ -42,6 +42,98 @@ RSpec.feature "MenusInterfaces", type: :feature do
         log_in_as @user
       }
 
+      context "and invalid menu" do
+        background {
+          @menu_date = ""
+          @menu_time = "夕食"
+          @dish_category = "主菜"
+          @dish_name = "鮭"
+          visit root_url
+          fill_in "menu_date", with: @menu_date
+          select @menu_time, from: "menu_time"
+          select @dish_category, from: "dish_category"
+          fill_in "dish_name", with: @dish_name
+          click_button "送信"
+        }
+
+        scenario "render root_url" do
+          expect(page).to have_current_path "/menus"
+        end
+
+        scenario "display danger message" do
+          expect(page).to have_selector(".alert-danger")
+        end
+
+        scenario "enter the same date in the form as last time" do
+          expect(page).to have_field "menu_date", with: @menu_date
+        end
+
+        scenario "enter the same time in the form as last time" do
+          expect(page).to have_select "menu_time", selected: @menu_time
+        end
+
+        scenario "enter the same dish_category in the form as last time" do
+          expect(page).to have_select "dish_category", selected: @dish_category
+        end
+
+        scenario "enter the same dish_name in the form as last time" do
+          expect(page).to have_field "dish_name", with: @dish_name
+        end
+
+        scenario "is accepted normally if content is fixed after invalid register" do
+          @menu_date = Date.yesterday
+          fill_in "menu_date", with: @menu_date
+          click_button "送信"
+          expect(page).to have_selector(".alert-success")
+        end
+      end
+
+      context "and invalid dish" do
+        background {
+          @menu_date = Date.yesterday
+          @menu_time = "夕食"
+          @dish_category = "主菜"
+          @dish_name = "a" * 31
+          visit root_url
+          fill_in "menu_date", with: @menu_date
+          select @menu_time, from: "menu_time"
+          select @dish_category, from: "dish_category"
+          fill_in "dish_name", with: @dish_name
+          click_button "送信"
+        }
+
+        scenario "render root_url" do
+          expect(page).to have_current_path "/menus"
+        end
+
+        scenario "display danger message" do
+          expect(page).to have_selector(".alert-danger")
+        end
+
+        scenario "enter the same date in the form as last time" do
+          expect(page).to have_field "menu_date", with: @menu_date
+        end
+
+        scenario "enter the same time in the form as last time" do
+          expect(page).to have_select "menu_time", selected: @menu_time
+        end
+
+        scenario "enter the same dish_category in the form as last time" do
+          expect(page).to have_select "dish_category", selected: @dish_category
+        end
+
+        scenario "enter the same dish_name in the form as last time" do
+          expect(page).to have_field "dish_name", with: @dish_name
+        end
+
+        scenario "is accepted normally if content is fixed after invalid register" do
+          @dish_name = "鮭"
+          fill_in "dish_name", with: @dish_name
+          click_button "送信"
+          expect(page).to have_selector(".alert-success")
+        end
+      end
+
       context "and new menu" do
         background {
           @menu_date = Date.yesterday
@@ -70,6 +162,11 @@ RSpec.feature "MenusInterfaces", type: :feature do
         scenario "redirect to root_url" do
           click_button "送信"
           expect(page).to have_current_path root_url
+        end
+
+        scenario "display success message" do
+          click_button "送信"
+          expect(page).to have_selector(".alert-success")
         end
 
         scenario "display the menu in the menu registeration list" do
@@ -122,6 +219,11 @@ RSpec.feature "MenusInterfaces", type: :feature do
         scenario "redirect to root_url" do
           click_button "送信"
           expect(page).to have_current_path(root_url)
+        end
+
+        scenario "display success message" do
+          click_button "送信"
+          expect(page).to have_selector(".alert-success")
         end
 
         scenario "display the dish in the menu registeration list" do
