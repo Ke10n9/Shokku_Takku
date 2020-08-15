89efae1b0954f987a6696ac7ada0a96c372153cd
Nagai Kensuke <kn.10n9@gmail.com>
2020-08-15 09:49:57 +0900

    Adjusting test error

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   # render_views
-  let(:base_title) { 'Shokku Takku' }
+  let(:base_title) { 'ダテさん -献立記録サービス-' }
 
     describe "Home" do
       before { get root_path }
@@ -10,8 +10,8 @@ RSpec.describe "StaticPages", type: :request do
       it "returns http success" do
         expect(response).to have_http_status(:success)
       end
-      
-      it "has title 'Shokku Takku'" do
+
+      it "has title 'ダテさん -献立記録サービス-'" do
         expect(response.body).to match(/<title>#{base_title}<\/title>/i)
       end
     end
