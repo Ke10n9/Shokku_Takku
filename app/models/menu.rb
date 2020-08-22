8df168a32ec2a6953985fba6481949d7709659df
Nagai Kensuke <kn.10n9@gmail.com>
2020-08-22 11:08:56 +0900

    Change recent menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -6,4 +6,14 @@ class Menu < ApplicationRecord
   validates :user_id, presence: true
   validates :date, presence: true
   validates :time, presence: true, uniqueness: { scope: [:user_id, :date] }
+  validate :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "5MB以上の画像はアップロードできません。")
+      end
+    end
 end
