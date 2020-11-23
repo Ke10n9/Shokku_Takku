9aa54e90b3568d71d4f31d12cf8ade79ba1aca55
Nagai Kensuke <kn.10n9@gmail.com>
2020-11-23 09:41:08 +0900

    bundle install 'mail-iso-2022-jp'

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,7 +6,7 @@ class AccountActivationsController < ApplicationController
       user.activate
       log_in user
       flash[:success] = "アカウントは有効化されました。"
-      redirect_to user
+      redirect_to root_url
     else
       flash[:danger] = "無効な有効化リンクです。"
       redirect_to root_url
