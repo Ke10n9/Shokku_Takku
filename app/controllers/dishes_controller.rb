52e54aaf9b1cf939f0cc416d9ef8ca8dffd21cdb
Nagai Kensuke <kn.10n9@gmail.com>
2020-11-26 18:44:43 +0900

    Make simple menu search background

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,13 @@
 class DishesController < ApplicationController
+
+  def index
+    @search_params =dish_search_params
+    @dishes = Dish.search(@search_params)
+  end
+
+  private
+
+    def dish_search_params
+      params.fetch(:search, {}).permit(:name)
+    end
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,13 @@
 class Dish < ApplicationRecord
   belongs_to :menu
-  validates :name, presence: true, length: { maximum: 30 }#,
-                  # uniqueness: { scope: :menu_id }
+  validates :name, presence: true, length: { maximum: 30 }
   validates :category, presence: true
+
+  scope :search, -> (search_params) do
+    return if search_params.blank?
+    name_like(search_params[:name])
+  end
+
+  # nameが存在する場合、nameをlike検索する
+  scope :name_like, -> (name) { where('name LIKE ?', "%#{name}%") if name.present? }
 end
