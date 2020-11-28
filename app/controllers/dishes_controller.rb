c3edf764daa1829f83bdf38bfaa5c50f38b34465
Nagai Kensuke <kn.10n9@gmail.com>
2020-11-28 21:28:40 +0900

    Complete menu search

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,10 +1,15 @@
 class DishesController < ApplicationController
   before_action :logged_in_user
+  before_action :set_menu_times
 
   def index
     @search_params = dish_search_params
-    @dishes = current_user.dishes.search(@search_params)
+    @dishes = current_user.dishes.search(@search_params).paginate(page: params[:page], per_page: 5)
 
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
