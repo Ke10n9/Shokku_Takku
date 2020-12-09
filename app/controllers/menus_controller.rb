b2cfcc19beb60d63106ed38b097796e2f575d540
Nagai Kensuke <kn.10n9@gmail.com>
2020-12-09 18:09:07 +0900

    Complete following-menu-list

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -109,7 +109,7 @@ class MenusController < ApplicationController
     @menus = Menu.page(params[:page]).per(30)
 
     @followings_menus = Menu.where(user: current_user.following)
-    @followings_menus = Kaminari.paginate_array(@followings_menus).page(params[:page]).per(10)
+    @followings_menus = Kaminari.paginate_array(@followings_menus).page(params[:page]).per(30)
   end
 
   private
