4c1a058d1024f8533fb9898e849922c68edc7995
Nagai Kensuke <kn.10n9@gmail.com>
2020-12-09 14:56:31 +0900

    Add tab for change menu list in everyone's menus

--- app/javascript/shared/tab.js
+++ app/javascript/shared/tab.js
@@ -1,9 +1,9 @@
-jQuery(function(){
-  jQuery('.tab li').on('click',function(){
-    jQuery('.tab li').removeClass("active");
-    jQuery(this).addClass("active");
-    var id=jQuery(this).attr('id');
-    jQuery('.tab_cnt div').removeClass("active");
-    jQuery('#'+id+'_cnt').addClass("active");
+$(document).on('turbolinks:load',function(){
+  $('.tab li').on('click',function(){
+    $('.tab li').removeClass("active");
+    $(this).addClass("active");
+    var id=$(this).attr('id');
+    $('.tab_cnt div').removeClass("active");
+    $('#'+id+'_cnt').addClass("active");
   });
 });
