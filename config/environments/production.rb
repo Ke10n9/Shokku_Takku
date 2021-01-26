c2f02c55b75b6232995c7425e0496efd78159ce5
Nagai Kensuke <kn.10n9@gmail.com>
2021-01-26 23:29:14 +0900

    Edit config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD'],
+    user_name: 'datesan.mail@gmail.com',
+    password: 'adxbkwdwpjtjyess',
     authentication: 'plain',
     enable_starttls_auto: true
   }
