d267b6cb1cd564fcca6905b742c3d350e3a5627f
Nagai Kensuke <kn.10n9@gmail.com>
2021-01-26 23:17:44 +0900

    Edit env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,10 +67,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'datesan.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     # :port           => ENV['MAILGUN_SMTP_PORT'],
     # :address        => ENV['MAILGUN_SMTP_SERVER'],
