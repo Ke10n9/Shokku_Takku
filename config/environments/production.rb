b579cf1cbcf0802387a0a92a2291d9fec72fb28a
Nagai Kensuke <kn.10n9@gmail.com>
2021-01-26 20:46:24 +0900

    Edit config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'datesan.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  # host = 'datesan.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     # :port           => ENV['MAILGUN_SMTP_PORT'],
     # :address        => ENV['MAILGUN_SMTP_SERVER'],
@@ -82,7 +82,7 @@ Rails.application.configure do
     port: 587,
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD'],
-    authentication: 'plain',
+    authentication: 'login',
     enable_starttls_auto: true
   }
 
