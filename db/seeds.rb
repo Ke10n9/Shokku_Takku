f1e0e44f00240458d895deb7d3fd80c8d6d4b8a0
Nagai Kensuke <kn.10n9@gmail.com>
2021-02-28 18:13:18 +0900

    Fix home layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
-              email: "example@railstutorial.org",
-              password:              "foobar",
-              password_confirmation: "foobar",
-              admin: true,
-              activated: true,
-              activated_at: Time.zone.now)
+# User.create!(name:  "Example User",
+#               email: "example@railstutorial.org",
+#               password:              "foobar",
+#               password_confirmation: "foobar",
+#               admin: true,
+#               activated: true,
+#               activated_at: Time.zone.now)
 
 User.create!(name:  "Test User",
               email: "test@railstutorial.org",
